fe6bf9d1e8a4f814cefa535baa95e5b03696e816
emily <emily@bacheconsulting.com>
2022-03-30 11:45:34 +0200

    first solution attempt

--- python/html_converter.py
+++ python/html_converter.py
@@ -2,11 +2,15 @@ import html as html_converter
 
 class UnicodeFileToHtmlTextConverter:
 
-    def __init__(self, full_filename_with_path):
+    def __init__(self, full_filename_with_path=None, stream=None):
         self.full_filename_with_path = full_filename_with_path
+        self.stream = stream
 
     def convert_to_html(self):
-        f = open(self.full_filename_with_path, "r")
+        if self.full_filename_with_path:
+            f = open(self.full_filename_with_path, "r")
+        else:
+            f = self.stream
         html = ""
         for line in f:
             line = line.rstrip()
--- python/tests/test_html_converter.py
+++ python/tests/test_html_converter.py
@@ -1,8 +1,17 @@
+import io
+
 import pytest
 
 from html_converter import UnicodeFileToHtmlTextConverter
 
 
 def test_html_converter():
-    converter = UnicodeFileToHtmlTextConverter("foo")
+    converter = UnicodeFileToHtmlTextConverter(full_filename_with_path="foo")
     assert "foo" == converter.full_filename_with_path
+
+
+def test_convert():
+    fake_file = io.StringIO("""foobar\n '' \n " """)
+    converter = UnicodeFileToHtmlTextConverter(stream=fake_file)
+    result = converter.convert_to_html()
+    assert result == "foobar<br /> &#x27;&#x27;<br /> &quot;<br />"
