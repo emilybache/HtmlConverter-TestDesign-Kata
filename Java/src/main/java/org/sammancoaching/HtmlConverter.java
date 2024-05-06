295ac438cd43b760cf34b3d26432a530bed110c7
emily <emily@bacheconsulting.com>
2024-05-06 15:01:10 +0200

    update to newer java version

--- Java/src/main/java/org/sammancoaching/HtmlConverter.java
+++ Java/src/main/java/org/sammancoaching/HtmlConverter.java
@@ -4,18 +4,21 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import static org.sammancoaching.StringEscapeUtils.escapeHtml;
+
 public class HtmlConverter {
 
     public String convertToHtml(String filename) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        var in = new FileReader(filename);
+        BufferedReader reader = new BufferedReader(in);
         String line = reader.readLine();
         // bug: should be "<html>"
-        StringBuilder html = new StringBuilder("<html<");
+        var html = new StringBuilder("<html<");
         while (line != null)
         {
             html.append("<p>");
             line = line.trim();
-            html.append(StringEscapeUtils.escapeHtml(line));
+            html.append(escapeHtml(line));
             // bug: should be "</p>"
             html.append("<br />");
             line = reader.readLine();
