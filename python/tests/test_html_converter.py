c85fe2df8bc3d3bd0d70f6c2a6a727be720cdde6
emily <emily@bacheconsulting.com>
2024-05-17 16:51:17 +0200

    Add stub file in python

--- python/tests/test_html_converter.py
+++ python/tests/test_html_converter.py
@@ -13,6 +13,21 @@ def TestDoubleFile2(text):
     return text.splitlines(True)
 
 
+class StubFile:
+    def __init__(self):
+        self.current = -1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.current += 1
+        if self.current == 0:
+            return " line 1"
+        if self.current == 1:
+            return " line 2  "
+        raise StopIteration
+
 def test_html_converter_uses_valid_html_tags():
     text = ""
     file = TestDoubleFile(text)
