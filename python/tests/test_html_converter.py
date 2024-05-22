0b9563fc7cdf2cf788b5a1efccf36ea8b1090b74
emily <emily@bacheconsulting.com>
2024-05-22 09:39:37 +0200

    add more test double options

--- python/tests/test_html_converter.py
+++ python/tests/test_html_converter.py
@@ -5,15 +5,51 @@ import pytest
 from html_converter import HtmlConverter
 
 
-def TestDoubleFile(text):
+def FakeFile_v1(text):
     return io.StringIO(text)
 
 
-def TestDoubleFile2(text):
+def FakeFile_v2(text):
     return text.splitlines(True)
 
 
-class StubFile:
+def test_html_converter_uses_valid_html_tags():
+    text = ""
+    file = FakeFile_v1(text)
+    result = HtmlConverter().convert_file_contents_to_html(file)
+    assert result == "<html></html>"
+
+
+def test_html_converter_uses_valid_paragraph_tags():
+    text = """\
+    line 1
+    line 2   """
+    file = FakeFile_v1(text)
+    result = HtmlConverter().convert_file_contents_to_html(file)
+    assert result == "<html><p>line 1</p><p>line 2</p></html>"
+
+
+def test_html_converter_uses_valid_html_tags_with_stub():
+    file = StubEmptyFile()
+    result = HtmlConverter().convert_file_contents_to_html(file)
+    assert result == "<html></html>"
+
+
+def test_html_converter_uses_valid_paragraph_tags_with_stub():
+    file = StubTwoLineFile()
+    result = HtmlConverter().convert_file_contents_to_html(file)
+    assert result == "<html><p>line 1</p><p>line 2</p></html>"
+
+
+class StubEmptyFile:
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        raise StopIteration
+
+
+class StubTwoLineFile:
     def __init__(self):
         self.current = -1
 
@@ -28,17 +64,3 @@ class StubFile:
             return " line 2  "
         raise StopIteration
 
-def test_html_converter_uses_valid_html_tags():
-    text = ""
-    file = TestDoubleFile(text)
-    result = HtmlConverter().convert_file_contents_to_html(file)
-    assert result == "<html></html>"
-
-
-def test_html_converter_uses_valid_paragraph_tags():
-    text = """\
-    line 1
-    line 2   """
-    file = TestDoubleFile(text)
-    result = HtmlConverter().convert_file_contents_to_html(file)
-    assert result == "<html><p>line 1</p><p>line 2</p></html>"
