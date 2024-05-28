aae616cc386806f9fc0fcd07a574567a58152fd8
gsl-anthonymerle <anthony.merle@groupeseloger.com>
2024-05-28 15:30:04 +0200

    Add swift project for the kata

---
+++ swift/HtmlConverter/Sources/HtmlConverter.swift
@@ -0,0 +1,15 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+// 
+// Swift Argument Parser
+// https://swiftpackageindex.com/apple/swift-argument-parser/documentation
+
+import ArgumentParser
+
+@main
+struct HtmlConverter: ParsableCommand {
+
+    mutating func run() throws {
+        print("Hello, world!")
+    }
+}
