eeed03dd15dac358ff0740b6e2b83b7ce31f6f6c
gsl-anthonymerle <anthony.merle@groupeseloger.com>
2024-05-28 15:53:11 +0200

    Add HttpUtility class

--- swift/HtmlConverter/Sources/HttpUtility.swift
+++ swift/HtmlConverter/Sources/HttpUtility.swift
@@ -2,6 +2,10 @@ import Foundation
 
 class HttpUtility {
     static func htmlEncode(_ line: String) -> String {
-        line
+        line.replacingOccurrences(of: "<", with: "&lt;")
+            .replacingOccurrences(of: ">", with: "&gt;")
+            .replacingOccurrences(of: "&", with: "&amp;")
+            .replacingOccurrences(of: "\"", with: "&quot;")
+            .replacingOccurrences(of: "\'", with: "&quot;")
     }
 }
