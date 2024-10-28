6e9188280aa734a33d3c6acaba47509d25ecc406
emily <emily@bacheconsulting.com>
2024-10-28 09:51:58 +0100

    original exercise in typescript

---
+++ TypeScript/text-converter-tests/unicode-file-to-html-text-converter-test.ts
@@ -0,0 +1,17 @@
+import { expect } from 'chai';
+import 'mocha';
+import UnicodeFileToHtmlTextConverter from '../text-converter/unicode-file-to-html-text-converter';
+
+describe('Html Converter', () => {
+
+	describe('HtmlTextConverter', () => {
+
+		it('foo', () => {
+			// TODO: Write a better test
+			// const converter = new UnicodeFileToHtmlTextConverter('foo');
+			// expect(converter.getFilename()).to.eql('fixme');
+		});
+
+	});
+
+});
